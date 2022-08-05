1ae292799ec047baaa1ae6f2d4e5d0bb16cec52e
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-08-05 17:56:28 +0500

    update

---
+++ app.js
@@ -0,0 +1,32 @@
+var mobilePhonesData = {
+    iphones: {
+        Pro13: {
+            nAme: "Apple iPhone 13 Pro Max",
+            diSplay: "6.7 inches (17.02 cm)",
+            battery: "4352 mAh",
+            colors: ["black", "white", "Gold"],
+            RAm: "6GB",
+        }
+        Pro12: {
+            nAme: "Apple iPhone 13 Pro Max",
+            diSplay: "6.7 inches (17.02 cm)",
+            battery: "4352 mAh",
+            colors: ["black", "white", "Gold"],
+            RAm: "6GB",
+        }
+        Pro11: {
+            nAme: "Apple iPhone 13 Pro Max",
+            diSplay: "6.7 inches (17.02 cm)",
+            battery: "4352 mAh",
+            colors: ["black", "white", "Gold"],
+            RAm: "6GB",
+        }
+        ProX: {
+            nAme: "Apple iPhone 13 Pro Max",
+            diSplay: "6.7 inches (17.02 cm)",
+            battery: "4352 mAh",
+            colors: ["black", "white", "Gold"],
+            RAm: "6GB",
+        }
+    }
+}
