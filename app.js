17d2cd0e5a4a092f38deab6e1b8c3d48042426d2
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-08-11 02:12:25 +0500

    update

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
-var mobilePhonesData = {
-    iphones: {
-        Pro13: {
-            nAme: "Apple iPhone 13 Pro Max",
-            diSplay: "6.7 inches (17.02 cm)",
-            battery: "4352 mAh",
-            colors: ["black", "white", "Gold"],
-            RAm: "6GB",
-        }
-        Pro12: {
-            nAme: "Apple iPhone 13 Pro Max",
-            diSplay: "6.7 inches (17.02 cm)",
-            battery: "4352 mAh",
-            colors: ["black", "white", "Gold"],
-            RAm: "6GB",
-        }
-        Pro11: {
-            nAme: "Apple iPhone 13 Pro Max",
-            diSplay: "6.7 inches (17.02 cm)",
-            battery: "4352 mAh",
-            colors: ["black", "white", "Gold"],
-            RAm: "6GB",
-        }
-        ProX: {
-            nAme: "Apple iPhone 13 Pro Max",
-            diSplay: "6.7 inches (17.02 cm)",
-            battery: "4352 mAh",
-            colors: ["black", "white", "Gold"],
-            RAm: "6GB",
-        }
-    }
-}
+// var search = prompt("Search Mobile Phones");
+// var mobilePhonesData = {
+//     Pro13: {
+//         nAme: "Apple iPhone 13 Pro Max",
+//         diSplay: "6.7 inches (17.02 cm)",
+//         battery: "4500 mAh",
+//         colors: ["black", "white", "Gold"],
+//         RAm: "6GB",
+//     }
+// };
+// console.log(mobilePhonesData.search);
+// Runner.prototype.gameOver = function(){}
+
+// var prp = prompt("Enter Something");
+
+var inner = document.getElementsByTagName("textarea");
+// var parentDiv = document.getElementById("div1");
+var newParagraph = document.createElement("h1");
+var t = document.createTextNode(inner);
+newParagraph.appendChild(t);
+paragraph1 = inner.firstChild;
+inner.insertBefore(newParagraph, paragraph1);
