d0d3e53e1b4d694c06eee448e4d5a91c6d51fba3
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-08-12 12:26:57 +0500

    update

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var search = prompt("Search Mobile Phones");
 var mobilePhonesData = {
     Pro13: {
-        nAme: "Apple iPhone 13 Pro Max",
-        diSplay: "6.7 inches (17.02 cm)",
+        name: "Apple iPhone 13 Pro Max",
+        display: "6.7 inches (17.02 cm)",
         battery: "4500 mAh",
         colors: ["black", "white", "Gold"],
-        RAm: "6GB",
+        ram: "6GB",
     }
 };
-console.log(mobilePhonesData.search);
+console.log(mobilePhonesData.Pro13[search]);
+
+// var iphone = {
+//     name: "my iphone",
+//     price: 12554,
+// };
+
+// console.log(iphone[search]);
