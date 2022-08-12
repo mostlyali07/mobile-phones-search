1fa6b9a316831b053cb3cbc38ddf848409e2b51b
Syed Ali Shah <104094853+mostlyali07@users.noreply.github.com>
2022-08-12 14:37:15 +0500

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function search() {
     var seArch = prompt("Search Mobile Phones");
     var apple = {
             name: "Apple iPhone 13 Pro Max",
-            display: "6.7 inches (17.02 cm)",
+            display: "6.7' 1284x2778 pixels",
             battery: "4500 mAh",
             colors: ["black", "white", "Gold"],
             ram: "6GB"
@@ -12,7 +12,49 @@ function search() {
         display: "7.6' 1768x2208 pixels",
         battery:"4400mAh",
         colors: ["black", "white", "Gold"],
-        ram: "6GB"
+        ram: "12GB"
+    };
+    var google ={
+        name: "Google Pixel 6 Pro",
+        display: "6.71' 1440x3120 pixels",
+        battery:"5003mAh",
+        colors: ["black", "white", "Gold"],
+        ram: "12GB"
+    };
+    var oppo ={
+        name: "Oppo Find X5 Pro",
+        display: "6.7' 1440x3216 pixels",
+        battery:"5000mAh",
+        colors: ["black", "white", "Gold"],
+        ram: "8 / 12GB"
+    };
+    var samsung ={
+        name: "Samsung Galaxy Z Fold3 5G",
+        display: "7.6' 1768x2208 pixels",
+        battery:"4400mAh",
+        colors: ["black", "white", "Gold"],
+        ram: "12GB"
+    };
+    var vivo ={
+        name: "vivo X80 Pro",
+        display: "7.6' 1768x2208 pixels",
+        battery:"4400mAh",
+        colors: ["black", "white", "Gold"],
+        ram: "12GB"
+    };
+    var realme ={
+        name: "Realme 9",
+        display: "7.6' 1768x2208 pixels",
+        battery:"4400mAh",
+        colors: ["black", "white", "Gold"],
+        ram: "12GB"
+    };
+    var samsung ={
+        name: "Samsung Galaxy Z Fold3 5G",
+        display: "7.6' 1768x2208 pixels",
+        battery:"4400mAh",
+        colors: ["black", "white", "Gold"],
+        ram: "12GB"
     };
     // console.log(aPPle.Pro13[seArch]);
 }
